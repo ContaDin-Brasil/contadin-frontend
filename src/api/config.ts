0196c15e3c3b08dce5354d41e50c56dd7cc364b1
Gustavo Kohatsu <gustavo.kohatsu@sptech.school>
2026-05-02 23:22:56 -0300

    update configs pra rodar local

--- src/api/config.ts
+++ src/api/config.ts
@@ -47,7 +47,7 @@ const getBaseURL = (): string => {
       // Android emulator acessa localhost da máquina host por 10.0.2.2
       baseURL = androidEmulatorHost;
     } else {
-      baseURL = "http://localhost:8080"
+      baseURL = "http://localhost:8080";
     }
   } else {
     baseURL = "https://api.seudominio.com";
